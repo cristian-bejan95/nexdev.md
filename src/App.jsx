549c16cede0c926912ebfe952eca2e772c0dc34a
Cristian <clickteh@gmail.com>
2025-03-11 10:48:10 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
 	return (
-		<div className="container">
-			<h1>NextDev</h1>
+		<div className="container h-screen mx-auto flex flex-col justify-center items-center p-5 gap-10">
+			<div className="flex flex-col gap-5">
+				<div className="flex items-center gap-2">
+					<h1 class="text-white text-5xl">NexDev</h1>
+				</div>
+				<p className="text-white text-3xl font-medium">Web Development and Maintenance.</p>
+			</div>
 		</div>
 	)
 }
