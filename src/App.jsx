fedf3297eac7d6beda8c83ff154a4e8661088bc5
Cristin Bejan <cristin.bejan@STISC.GOV.AD>
2025-03-11 13:35:32 +0200

    Nexdev

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 function App() {
 	return (
-		<div className="container h-screen mx-auto flex flex-col justify-center items-center p-5 gap-10">
-			<div className="flex flex-col gap-5">
-				<div className="flex items-center gap-2">
-					<h1 class="text-white text-5xl">NexDev</h1>
+		<div>
+			<div className="container h-screen mx-auto flex flex-col justify-center items-center p-5 gap-10">
+				<div className="flex flex-col gap-5">
+					<div className="flex items-center gap-2">
+						<h1 className="title">NexDev</h1>
+					</div>
+				<p className="descr">Web Development and Maintenance.</p>
 				</div>
-				<p className="text-white text-3xl font-medium">Web Development and Maintenance.</p>
 			</div>
 		</div>
 	)
