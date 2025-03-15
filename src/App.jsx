a407034a36510e82cee284e2b05eefb38c54b80e
Cristin Bejan <cristin.bejan@STISC.GOV.AD>
2025-03-15 11:10:19 +0200

    NexDev

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
 function App() {
 	return (
 		<div>
-			<div className="container h-screen mx-auto flex flex-col justify-center items-center p-5 gap-10">
-				<div className="flex flex-col gap-5">
-					<div className="flex items-center gap-2">
+			<div className="container flex flex-col items-start justify-center h-screen gap-10 p-5 mx-auto">
+				<div className="flex flex-col gap-2">
+					<div className="flex items-center">
+						<div href="/" className="logo">
+							<img src="./images/nexdev.svg" width={130} alt="" />
+						</div>
 						<h1 className="title">NexDev</h1>
 					</div>
-				<p className="descr">Web Development and Maintenance.</p>
+					<p className="descr">Web Development and Maintenance.</p>
+				</div>
+				<div className="flex flex-col gap-3 text-white">
+					<a href="tel:37368589463" className="relative flex items-center gap-3 text-xl transition duration-200 hover:translate-x-3">
+						<FaPhoneAlt />
+						+373 68 589 463
+					</a>
+					<a href="mailto:support@nexdev.md" className="relative flex items-center gap-3 text-xl transition duration-200 hover:translate-x-3">
+						<FaEnvelope />
+						support@nexdev.md
+					</a>
+					<a href="https://www.instagram.com/nexdev.md/" className="relative flex items-center gap-3 text-xl transition duration-200 hover:translate-x-3">
+						<FaInstagram />
+						nexdev.md
+					</a>
 				</div>
 			</div>
 		</div>
